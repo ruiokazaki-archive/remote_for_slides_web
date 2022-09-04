1972bb0ee439cedcbafe55c9685dd64dec8b8bbd
OkazakiRui <okazaki200226@gmail.com>
2022-09-05 05:34:49 +0900

    feat: create index page

---
+++ public/script/index.js
@@ -0,0 +1,27 @@
+const formElement = document.querySelector('form');
+const inputElement = document.querySelector('input');
+
+const checkValid = async (roomId) => {
+  const res = await fetch(`/check-valid/${roomId}`);
+  const data = await res.json();
+
+  if (data.status !== 200) {
+    throw new Error(data.message);
+  }
+
+  return data;
+};
+
+formElement.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const roomId = inputElement.value;
+
+  try {
+    const data = await checkValid(roomId);
+    inputElement.value = '';
+    window.location.href = `/room/${data.roomId}`;
+  } catch (e) {
+    alert(e);
+    console.error(e);
+  }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
-import bodyParser from "body-parser";
-import express from "express";
+import bodyParser from 'body-parser';
+import express from 'express';
 
 const app = express();
 const port = process.env.PORT || 3333;
 
+app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
 
-app.get("/", async (req, res) => {
-  res.json({ Hello: "World" });
+// web route
+app.get('/', async (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// api route
+app.get('/check-valid/:roomId', async (req, res) => {
+  res.json({ status: 200, roomId: req.params.roomId });
+  // res.json({ status: 403, message: 'Invalid room id' });
 });
 
 app.listen(port, () => {
