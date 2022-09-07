8addd08cc4900d856344dbfa3fff5ce105fcdbeb
yumekiti <yumekiti120440@gmail.com>
2022-09-07 21:30:40 +0900

    `add` socket client

--- public/script/room.js
+++ public/script/room.js
@@ -1,10 +1,27 @@
 const [_, _room, roomId] = location.pathname.split('/');
-console.log({ roomId });
+const socket = io.connect(location.host);
+socket.emit("join", roomId);
+
+socket.on("connect", () => {
+  console.log("socket connected");
+});
+
+socket.on("event", (value) => {
+  console.log(value);
+});
 
 const [prevButton, nextButton] = document.querySelectorAll('button');
 prevButton.addEventListener('click', () => {
-  console.log('prev');
+  data = {
+    uuid: roomId,
+    event: 'prev'
+  }
+  socket.emit("event", data);
 });
 nextButton.addEventListener('click', () => {
-  console.log('next');
+  data = {
+    uuid: roomId,
+    event: 'next'
+  }
+  socket.emit("event", data);
 });
